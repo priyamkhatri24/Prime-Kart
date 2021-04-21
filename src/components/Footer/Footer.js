8a883a1912b5d70ff1b8e9500b561760b46540f9
Priyam Khatri <priyamkhatri24@gmail.com>
2021-04-21 15:53:34 +0530

    favicon added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,11 @@ const footer = () => {
     <div className={classes.footer}>
       <div className={classes.column}>
         <h3>About</h3>
-        <p>Flipkart Clone Project Designed and made by Priyam Khatri.</p>
+        <p>
+          Flipkart Clone Project Designed and made by Priyam Khatri. This is not
+          an antual shopping e-commerce website. This is a clone of already
+          exisiting e-commerce website.
+        </p>
       </div>
       <div className={classes.column}>
         <h3>Lets be Social</h3>
